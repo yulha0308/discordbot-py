8ef82eadf2a7e4d5c2c14fe196c9183fc5ffa405
yulha0308 <121702185+yulha0308@users.noreply.github.com>
2022-12-31 11:38:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,32 +1,68 @@
 from cmath import log
 from distutils.sysconfig import PREFIX
-import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord.utils import get
 import os
+
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
+intents=discord.Intents.default()
+intents.message_content=True
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}.')
+    
+@bot.command()
+async def 고정링크(ctx):
+  await ctx.send('https://discord.gg/z9Za2WW6AK%27.format(ctx.author.mention))
+                 
+role='멤버'
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def 서버가입(ctx, member: discord.Member=None):
+  global user_role
+  user_role=role
+  member = member or ctx.message.author
+  await member.add_roles(get(ctx.guild.roles, name=user_role))
+  await ctx.channel.send("{}님이 정상적으로 서버가입을 마쳤습니다.".format(ctx.author.mention))
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
-
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
 
 
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
     print("Improper token has been passed.")
+
+    
+    
+'''import discord
+
+import math
+
+intents=discord.Intents.default()
+intents.message_content=True
+
+bot=commands.Bot(command_prefix='읏추야 ', intents=intents)
+
+@bot.command()
+async def 고정링크(ctx):
+  await ctx.send('https://discord.gg/z9Za2WW6AK%27.format(ctx.author.mention))
+
+role='멤버'
+
+@bot.command()
+async def 서버가입(ctx, member: discord.Member=None):
+  global user_role
+  user_role=role
+  member = member or ctx.message.author
+  await member.add_roles(get(ctx.guild.roles, name=user_role))
+  await ctx.channel.send("{}님이 정상적으로 서버가입을 마쳤습니다.".format(ctx.author.mention))
+
+bt='MTA1MDM5NTc2MjQ4OTU3MzQ0Ng.G6dzu6.1h3Z0XQn-aD17QnYu1O2JpuXM9n_Lfgm_ikwOM'
+bot.run(bt)'''
