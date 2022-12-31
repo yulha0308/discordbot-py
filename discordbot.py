4a9d8e20d37b912f50e279afdeeff2b8fe3f69de
yulha0308 <121702185+yulha0308@users.noreply.github.com>
2022-12-31 11:39:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -40,29 +40,3 @@ except discord.errors.LoginFailure as e:
     print("Improper token has been passed.")
 
     
-    
-'''import discord
-
-import math
-
-intents=discord.Intents.default()
-intents.message_content=True
-
-bot=commands.Bot(command_prefix='읏추야 ', intents=intents)
-
-@bot.command()
-async def 고정링크(ctx):
-  await ctx.send('https://discord.gg/z9Za2WW6AK%27.format(ctx.author.mention))
-
-role='멤버'
-
-@bot.command()
-async def 서버가입(ctx, member: discord.Member=None):
-  global user_role
-  user_role=role
-  member = member or ctx.message.author
-  await member.add_roles(get(ctx.guild.roles, name=user_role))
-  await ctx.channel.send("{}님이 정상적으로 서버가입을 마쳤습니다.".format(ctx.author.mention))
-
-bt='MTA1MDM5NTc2MjQ4OTU3MzQ0Ng.G6dzu6.1h3Z0XQn-aD17QnYu1O2JpuXM9n_Lfgm_ikwOM'
-bot.run(bt)'''
